18ff4a9a217cea073221090e7679ab1dbfdbd40c
ikrong <lun.i@outlook.com>
2020-05-01 15:15:45 +0800

    换行

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 if (!(parseInt(process.versions.node.split('.')[0]) >= 10)) {
-    console.log('\u001b[91m需要node版本号10.X以上\u001b[39m')
+    console.log('\n\u001b[91m需要node版本号10.X以上\u001b[39m')
 } else {
     require('../dist/index.js')
 }
